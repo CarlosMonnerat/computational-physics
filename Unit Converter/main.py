f73fa09e569ba2773ef600f42322a33ed69e8284
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-29 15:43:36 -0300

    Create main.py

---
+++ Unit Converter/main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+
+#cores
+cor01 = '#3b3b3b' #Preta
+
+janela = Tk()
+janela.title('Unit Coverter')
+
+janela.geometry('650x260')
+janela.configure(bg = cor01)
+
+janela.mainloop()
