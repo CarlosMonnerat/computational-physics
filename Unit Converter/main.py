f80097aa1a1ec29f901dd1d49a4ace9cef595d47
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-09-29 16:08:36 -0300

    Update main.py

--- Unit Converter/main.py
+++ Unit Converter/main.py
@@ -1,13 +1,29 @@
 from tkinter import *
 from tkinter import ttk
 
-#cores
-cor01 = '#3b3b3b' #Preta
+#colors
+cor01 = '#3b3b3b'     # Black
+cor02 = '#ffffff'     # White
 
-janela = Tk()
-janela.title('Unit Coverter')
+window = Tk()
+window.title('Unit Coverter')
+window.geometry('650x260')
+window.configure(bg = cor01)
 
-janela.geometry('650x260')
-janela.configure(bg = cor01)
+# --------------------- Frames for Window --------------
+frame_up = Frame(window, width = 450, height = 50, bg = cor02, pady = 0, padx = 3, relief = 'flat')
+frame_up.place(x = 2, y = 2)
 
-janela.mainloop()
+frame_left = Frame(window, width = 450, height = 220, bg = cor02, pady = 0, padx = 3, relief = 'flat')
+frame_left.place(x = 2, y = 54)
+
+frame_right = Frame(window, width = 198, height = 260, bg = cor02, pady = 0, padx = 3, relief = 'flat')
+frame_right.place(x = 454, y = 2)
+
+# --------------------- Styles for Window --------------
+style = ttk.Style(window)
+style.theme_use("clam")
+
+
+
+window.mainloop()
