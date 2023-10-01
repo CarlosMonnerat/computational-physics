763cbe53f1c700b0e7f38249c137610c8ec461de
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2023-10-01 13:20:45 -0300

    Update main.py

--- unit-converter/main.py
+++ unit-converter/main.py
@@ -14,7 +14,7 @@ window.title('Meson Pi Software')
 window.geometry('650x260')
 window.configure(bg = cor01)
 
-# --------------------- Frames for Window --------------
+# ----------------------------------- Frames for Window -----------------------------
 frame_up = Frame(window, width=450, height=50, bg=cor02, pady=0, padx=3, relief='flat')
 frame_up.place(x=2, y=2)
 
@@ -24,16 +24,18 @@ frame_left.place(x=2, y=54)
 frame_right = Frame(window, width= 198, height=260, bg=cor02, pady=0, padx=3, relief='flat')
 frame_right.place(x=454, y=2)
 
-# --------------------- Styles for Window --------------
+# ------------------------------------- Styles for Window ----------------------------
 style = ttk.Style(window)
 style.theme_use("clam")
 
-# --------------------- Labels for Frame_up --------------
+# ------------------------------------ Labels for Frame_up -----------------------------
 l_app_name = Label(frame_up, text='Measurement Units Calculator', height=1, padx=0, relief='flat',
                     anchor='center', font=('Ivy 15 bold'), bg=cor02, fg=cor03)
 l_app_name.place(x=50, y=10)
 
-# --------------------- Settings Frame_left --------------
+
+
+# ------------------------------------ Settings Frame_left -------------------------------------
 # Weight Button
 img_0 = Image.open('icons/weight.png')
 img_0 = img_0.resize((50,50))
@@ -105,6 +107,15 @@ img_8 = ImageTk.PhotoImage(img_8)
 btn_8 = Button(frame_left, text='Pressure', image=img_8, compound=LEFT, width=130, height=50, relief='flat',
                overrelief='solid', anchor='nw', font=('Ivy 10 bold'), bg=cor03, fg=cor02)
 btn_8.grid(row=2, column=2, sticky=NSEW, pady=5, padx=5)
+
+
+# ------------------------------ Labels for Frame_right -----------------------------
+l_unit_name = Label(frame_right, text='Unit', width=16, height=2, padx=0, relief='groove',
+                    anchor='center', font=('Ivy 15 bold'), bg=cor02, fg=cor01)
+l_unit_name.place(x=0, y=0)
+
+
+# --------------------- Settings Frame_right --------------
 
 
 window.mainloop()
